f8227ba454b4f322b3479a7d3e18e83057ea56d7
AngelHw <angel.rg@aol.com>
2024-06-14 13:21:07 -0600

    Se agrega eliminación de paciente

---
+++ src/main/resources/static/js/paciente/delete_paciente.js
@@ -0,0 +1,17 @@
+function deleteBy(id){
+
+      if(window.confirm("¿Está seguro de eliminar el paciente con id: " + id +"?")){
+      //con fetch invocamos a la API de peliculas con el método DELETE
+      //pasandole el id en la URL
+      const url = 'paciente/'+ id;
+      const settings = {
+          method: 'DELETE'
+      }
+      fetch(url,settings)
+      .then(response => response.json())
+
+      //borrar la fila de la pelicula eliminada
+      let row_id = "#tr_" + id;
+      document.querySelector(row_id).remove();
+    }
+}
