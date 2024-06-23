e4ac2e0fe52fc46e0f701264de4baffb1478e696
AngelHw <angel.rg@aol.com>
2024-06-23 13:35:26 -0600

    pagina de error

--- src/main/java/Clinica/ClinicaOdontologica/controller/ErroresController.java
+++ src/main/java/Clinica/ClinicaOdontologica/controller/ErroresController.java
@@ -2,22 +2,25 @@ package Clinica.ClinicaOdontologica.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+public class ErroresController implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
+        System.out.println("Controlador de errores");
+
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
-            if(statusCode == HttpStatus.UNAUTHORIZED.value()) {
-                return "error-403";
+            if(statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "templates/errores/error-403";
             }
         }
-        return "error";
+        return "templates/errores/error";
     }
 }
